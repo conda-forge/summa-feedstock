30c6806eb4c75736826be068c05563b153892ff7
Andrew Bennett <bennett.andr@gmail.com>
2022-09-08 13:07:32 -0700

    Add flags back in?

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,16 +6,18 @@ if [[ $(uname) == Linux ]]; then
 fi
 
 export F_MASTER=$(pwd)
-# export FC=gfortran
 export FC_EXE=${FC}
 export FC_ENV=$(uname)
 export NCDF_PATH=${PREFIX}
-# export LIBLAPACK="$LDFLAGS -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -llapack -lblas"
+
 export INCLUDES='-I${PREFIX}/include -I/usr/include'
 export LIBRARIES='-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib -lnetcdff -llapack -lblas'
 
 export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
-
+export FLAGS_NOAH="-O3 -ffree-form -ffree-line-length-none -fmax-errors=0"
+export FLAGS_COMM="-O3 -ffree-line-length-none -fmax-errors=0"
+export FLAGS_SUMMA="-O3 -ffree-line-length-none -fmax-errors=0"
 export EXE_PATH="$PREFIX/bin"
+
 make -C ${F_MASTER}/build/ -f Makefile
 mv $F_MASTER/bin/summa.exe $PREFIX/bin/summa.exe
